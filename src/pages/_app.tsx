b813347f5880753541e61e3aff183412dade3652
Lucas :) <91512054+queirozlc@users.noreply.github.com>
2023-02-14 22:31:00 -0300

    Initial commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,15 @@
+import type { AppProps } from 'next/app'
+import GlobalStyle from '@/styles/global'
+import { ThemeProvider } from 'styled-components'
+import theme from '@/styles/themes/theme'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
+}
